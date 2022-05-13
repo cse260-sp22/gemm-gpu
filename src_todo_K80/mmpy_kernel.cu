47f0912a71df512de7bfa33d91990a077fe9d6ae
Brandon451 <47738722+Brandon451@users.noreply.github.com>
2022-05-13 10:59:00 -0700

    Added code from slides for shared memory
    Does not implement edge case handling for non sized blocks

--- src_todo_K80/mmpy_kernel.cu
+++ src_todo_K80/mmpy_kernel.cu
@@ -10,21 +10,53 @@ using namespace std;
 
 #include <stdio.h>
 
-__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
-{
+//__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
+//{
+//
+//    int I = blockIdx.y * blockDim.y + threadIdx.y;
+//    int J = blockIdx.x * blockDim.x + threadIdx.x;
+//
+//    if ((I < N) && (J < N))
+//    {
+//        _DOUBLE_ _c = 0;
+//        for (unsigned int k = 0; k < N; k++)
+//        {
+//            _DOUBLE_ a = A[I * N + k];
+//            _DOUBLE_ b = B[k * N + J];
+//            _c += a * b;
+//        }
+//        C[I * N + J] = _c;
+//    }
+//}
 
-    int I = blockIdx.y * blockDim.y + threadIdx.y;
-    int J = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
 
-    if ((I < N) && (J < N))
-    {
-        _DOUBLE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++)
-        {
-            _DOUBLE_ a = A[I * N + k];
-            _DOUBLE_ b = B[k * N + J];
-            _c += a * b;
-        }
-        C[I * N + J] = _c;
-    }
+	//local shared storage
+	__shared__ double As[TW][TW];
+	__shared__ double Bs[TW][TW];
+
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+	int bx = blockIdx.x;
+	int by = blockIdx.y;
+
+	int J = bx*TW + tx;
+	int I = by*TW + ty;
+
+	double Cij = 0;
+
+
+	for (int kk = 0; kk < N/TW; kk++){
+	
+		As[ty][tx] = A[I*N + kk*TW + tx];	
+		Bs[ty][tx] = A[(kk*TW+ty)*N + J];
+		__syncthreads();
+
+		for (int k = 0; k < TW; k++)
+				Cij += As[ty][k] * Bs[k][tx];
+		__syncthreads();
+	}
+
+	C[I*N + J] = Cij;
+
 }
