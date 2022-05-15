6f89ad8f19daae251dcb8c3b135a256bf858c778
Brandon451 <brandon.saldanha@gmail.com>
2022-05-15 09:28:14 +0000

    Added ILP with padding - n+1 not working :(

--- src_todo_K80/mmpy_kernel.cu
+++ src_todo_K80/mmpy_kernel.cu
@@ -10,6 +10,8 @@ using namespace std;
 
 #include <stdio.h>
 
+#define TW 32
+
 #define globA(x, y) A[x*N + y]
 #define globB(x, y) B[x*N + y]
 #define globC(x, y) C[x*N + y]
@@ -17,34 +19,46 @@ using namespace std;
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
 
 	//local shared storage
-	__shared__ double As[TW][TW];
-	__shared__ double Bs[TW][TW];
+	__shared__ _DOUBLE_ As[TW][TW];
+	__shared__ _DOUBLE_ Bs[TW][TW];
 
 	int tx = threadIdx.x;
-	int ty = threadIdx.y;
 	int bx = blockIdx.x;
+
+	int ty = threadIdx.y;
 	int by = blockIdx.y;
 
-	int J = bx*TW + tx;
-	int I = by*TW + ty;
+	int J = 2*bx*blockDim.x + tx;
+	int I = 2*by*blockDim.y + ty;
 
-	double Cij = 0;
+	_DOUBLE_ Cij[2][2] = {0};
 
 	for (int kk = 0; kk < (N+TW-1)/TW; kk++){
 	
-		if (I < N && (kk*TW + tx) < N) As[ty][tx] = globA(I, (kk*TW + tx));
-		else As[ty][tx] = 0;
+		if (I       < N	&& kk*TW + tx       < N) As[ty]		[tx] 	    = globA(I, 			(kk*TW + tx)); 		else As[ty]		[tx] 		= 0;
+		if (I + 16  < N && kk*TW + tx       < N) As[ty + 16][tx] 	    = globA((I + 16), 	(kk*TW + tx)); 		else As[ty + 16][tx] 		= 0;
+		if (I       < N && kk*TW + tx + 16  < N) As[ty]		[tx + 16]   = globA(I, 			(kk*TW + tx + 16)); else As[ty]		[tx + 16] 	= 0;
+		if (I + 16  < N && kk*TW + tx + 16  < N) As[ty + 16][tx + 16]   = globA((I + 16), 	(kk*TW + tx + 16)); else As[ty + 16][tx + 16] 	= 0;
 
-		if ((kk*TW + ty) < N && J < N) Bs[ty][tx] = globB((kk*TW+ty), J);
-		else Bs[ty][tx] = 0;
+		if (kk*TW + ty < N 		&& J < N) 		Bs[ty]		[tx] 		= globB((kk*TW+ty), 		J); 		else Bs[ty]		[tx] 		= 0;
+		if (kk*TW + ty + 16 < N && J < N) 		Bs[ty + 16]	[tx] 		= globB((kk*TW+ty + 16), 	J); 		else Bs[ty + 16][tx] 		= 0;
+		if (kk*TW + ty < N 		&& J + 16 < N) 	Bs[ty]		[tx + 16] 	= globB((kk*TW+ty), 		(J + 16)); 	else Bs[ty]		[tx + 16] 	= 0;
+		if (kk*TW + ty + 16 < N && J + 16 < N) 	Bs[ty + 16]	[tx + 16] 	= globB((kk*TW+ty + 16), 	(J + 16)); 	else Bs[ty + 16][tx + 16] 	= 0;
 		
 		__syncthreads();
 
-		for (int k = 0; k < TW; k++)
-			Cij += As[ty][k] * Bs[k][tx];
+		for (int k = 0; k < TW; k++){
+			Cij[0][0] += As[ty]		[k] * Bs[k][tx];
+			Cij[1][0] += As[ty + 16][k] * Bs[k][tx];
+			Cij[0][1] += As[ty]		[k] * Bs[k][tx + 16];
+			Cij[1][1] += As[ty + 16][k] * Bs[k][tx + 16];
+        }
 		__syncthreads();
 	}
 
-	if (I < N && J < N) globC(I, J) = Cij;
+	if (I < N 		&& J < N) 		globC(I,        J)          = Cij[0][0];
+	if (I + 16 < N 	&& J < N) 		globC((I + 16), J)          = Cij[1][0];
+	if (I < N 		&& J + 16 < N) 	globC(I,        (J + 16))   = Cij[0][1];
+	if (I + 16 < N 	&& J + 16 < N) 	globC((I + 16), (J + 16))   = Cij[1][1];
 
 }
