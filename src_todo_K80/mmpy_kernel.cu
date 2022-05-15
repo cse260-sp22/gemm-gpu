da690488ef0de140c7f9382de5a871b684ba1031
Brandon451 <brandon.saldanha@gmail.com>
2022-05-15 08:44:50 +0000

    Tested and working padding for all values

--- src_todo_K80/mmpy_kernel.cu
+++ src_todo_K80/mmpy_kernel.cu
@@ -11,26 +11,8 @@ using namespace std;
 #include <stdio.h>
 
 #define globA(x, y) A[x*N + y]
-#define globB(x, y) A[x*N + y]
-
-//__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
-//{
-//
-//    int I = blockIdx.y * blockDim.y + threadIdx.y;
-//    int J = blockIdx.x * blockDim.x + threadIdx.x;
-//
-//    if ((I < N) && (J < N))
-//    {
-//        _DOUBLE_ _c = 0;
-//        for (unsigned int k = 0; k < N; k++)
-//        {
-//            _DOUBLE_ a = A[I * N + k];
-//            _DOUBLE_ b = B[k * N + J];
-//            _c += a * b;
-//        }
-//        C[I * N + J] = _c;
-//    }
-//}
+#define globB(x, y) B[x*N + y]
+#define globC(x, y) C[x*N + y]
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
 
@@ -50,14 +32,10 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
 
 	for (int kk = 0; kk < (N+TW-1)/TW; kk++){
 	
-		if (I < N && (kk*TW + tx) < N){
-			As[ty][tx] = A[I*N + kk*TW + tx];
-		}
+		if (I < N && (kk*TW + tx) < N) As[ty][tx] = globA(I, (kk*TW + tx));
 		else As[ty][tx] = 0;
 
-		if ((kk*TW + ty) < N && J < N){
-			Bs[ty][tx] = B[(kk*TW+ty)*N + J];
-		}
+		if ((kk*TW + ty) < N && J < N) Bs[ty][tx] = globB((kk*TW+ty), J);
 		else Bs[ty][tx] = 0;
 		
 		__syncthreads();
@@ -67,7 +45,6 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
 		__syncthreads();
 	}
 
-	if (I < N && J < N)
-		C[I*N + J] = Cij;
+	if (I < N && J < N) globC(I, J) = Cij;
 
 }
