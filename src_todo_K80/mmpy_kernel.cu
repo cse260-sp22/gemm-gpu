8974170f1349bd2785935bca9c38387c31e2f722
Brandon451 <47738722+Brandon451@users.noreply.github.com>
2022-05-17 01:18:03 -0700

    New parameters - M, N, K: 32, 32, 16

--- src_todo_K80/mmpy_kernel.cu
+++ src_todo_K80/mmpy_kernel.cu
@@ -10,30 +10,28 @@ using namespace std;
 
 #include <stdio.h>
 
-#define TW 32
 #define Cy 32
 #define Cx 32
 #define Cc 32
-
 
 #define globA(x, y) __ldg(&A[x*N + y])
 #define globB(x, y) __ldg(&B[x*N + y])
 #define globC(x, y) C[x*N + y]
 
-__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
+__global__ void matMul(int N, _DOUBLE_ * __restrict C, _DOUBLE_ * __ restrict A, _DOUBLE_ * __ restrict B){
 
 	//local shared storage
 	__shared__ _DOUBLE_ As[Cy][Cc];
 	__shared__ _DOUBLE_ Bs[Cc][Cx];
 
 	int tx = threadIdx.x;
-	int bx = blockIdx.x;
+	int bx = 2*blockIdx.x;
 
 	int ty = threadIdx.y;
-	int by = blockIdx.y;
+	int by = 2*blockIdx.y;
 
-	int J = 2*bx*blockDim.x + tx;
-	int I = 2*by*blockDim.y + ty;
+	int J = bx*blockDim.x + tx;
+	int I = by*blockDim.y + ty;
 
 	_DOUBLE_ Cij[4] = {0};
 
@@ -41,13 +39,9 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
 	
 		if (I       < N	&& kk*Cc + tx       < N) As[ty]		[tx] 	    = globA(I, 			(kk*Cc + tx)); 		else As[ty]		[tx] 		= 0;
 		if (I + 16  < N && kk*Cc + tx       < N) As[ty + 16][tx] 	    = globA((I + 16), 	(kk*Cc + tx)); 		else As[ty + 16][tx] 		= 0;
-		if (I       < N && kk*Cc + tx + 16  < N) As[ty]		[tx + 16]   = globA(I, 			(kk*Cc + tx + 16)); else As[ty]		[tx + 16] 	= 0;
-		if (I + 16  < N && kk*Cc + tx + 16  < N) As[ty + 16][tx + 16]   = globA((I + 16), 	(kk*Cc + tx + 16)); else As[ty + 16][tx + 16] 	= 0;
 
 		if (kk*Cc + ty < N 		&& J < N) 		Bs[ty]		[tx] 		= globB((kk*Cc+ty), 		J); 		else Bs[ty]		[tx] 		= 0;
-		if (kk*Cc + ty + 16 < N && J < N) 		Bs[ty + 16]	[tx] 		= globB((kk*Cc+ty + 16), 	J); 		else Bs[ty + 16][tx] 		= 0;
 		if (kk*Cc + ty < N 		&& J + 16 < N) 	Bs[ty]		[tx + 16] 	= globB((kk*Cc+ty), 		(J + 16)); 	else Bs[ty]		[tx + 16] 	= 0;
-		if (kk*Cc + ty + 16 < N && J + 16 < N) 	Bs[ty + 16]	[tx + 16] 	= globB((kk*Cc+ty + 16), 	(J + 16)); 	else Bs[ty + 16][tx + 16] 	= 0;
 		
 		__syncthreads();
 
