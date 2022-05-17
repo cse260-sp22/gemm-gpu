d4d16fa6caf494252edf72cf5c9d6cfbe94722cc
Raghav Prasad <raghavprasad13@gmail.com>
2022-05-17 09:33:54 -0700

    change double to _DOUBLE_

--- src_todo_K80/mmpy_kernel.cu
+++ src_todo_K80/mmpy_kernel.cu
@@ -142,11 +142,11 @@ __global__ void matMul_old(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B){
 	}
 }
 
-__global__ void matMul(int N, _DOUBLE_ * __restrict C, _DOUBLE_ * __restrict A, _DOUBLE_ * __restrict B){
+__global__ void matMul(const int N, _DOUBLE_ * __restrict C, _DOUBLE_ * __restrict A, _DOUBLE_ * __restrict B){
 
 	//local shared storage
-	__shared__ double As[64][17];
-	__shared__ double Bs[16][64];
+	__shared__ _DOUBLE_ As[64][17];
+	__shared__ _DOUBLE_ Bs[16][64];
 
 	_DOUBLE_ Ar[4] 		= {0};
 	_DOUBLE_ Br[4] 		= {0};
