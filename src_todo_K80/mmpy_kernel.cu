5b1e9f53cbdab1dcdde07c630e53a4fee49d4a1c
Ubuntu <ubuntu@ip-172-31-34-194.us-west-2.compute.internal>
2022-05-18 03:03:13 +0000

    Bugfix in setGrid.cu

--- src_todo_K80/mmpy_kernel.cu
+++ src_todo_K80/mmpy_kernel.cu
@@ -20,7 +20,7 @@ using namespace std;
 #define globB(x, y) __ldg(&B[x*N + y])
 #define globC(x, y) C[x*N + y]
 
-__global__ void matMul(int N, _DOUBLE_ * __restrict C, _DOUBLE_ * __restrict A, _DOUBLE_ * __restrict B) {
+__global__ void matMul(int N, _DOUBLE_ * __restrict C, _DOUBLE_ * __restrict A, _DOUBLE_ * __restrict B){
 
 	//local shared storage
 	__shared__ _DOUBLE_ As[Cy][Cc];
